0091f186d14ce5ee5e6875c46cf643740fed92c5
creatorMao <zwmao@outlook.com>
2017-11-18 21:55:16 +0800

    player sample
    1. 媒体播放器sample

---
+++ Samples/PlayerSample/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace PlayerSample
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void PlayMyMedioElement(object sender, RoutedEventArgs e)
+        {
+            //从当前位置播放媒体
+            MyMedioElement.Play();
+            //从当前位置暂停媒体
+            //MyMedioElement.Pause();
+            //停止播放，并且重头开始
+            //MyMedioElement.Stop();
+        }
+    }
+}
